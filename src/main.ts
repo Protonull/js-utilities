ef10d506346753d44fed5d25b51f9fb978a0c3f1
Alexander <protonull@protonmail.com>
2023-04-24 17:17:53 +0100

    Deprecate main

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,9 @@
-// ------------------------------------------------------------ //
-// Miscellaneous
-// ------------------------------------------------------------ //
-
-import { exists, requireExistence } from "./lang";
+// This is a legacy entry file.
 export { exists, requireExistence } from "./lang";
-
-// ------------------------------------------------------------ //
-// Objects
-// ------------------------------------------------------------ //
-
-import { objectWithoutValues, objectApplyTemplate } from "./objects";
 export { objectWithoutValues, objectApplyTemplate } from "./objects";
-
-// ------------------------------------------------------------ //
-// Strings
-// ------------------------------------------------------------ //
-
-import { ensureString, stringEqualsIgnoreCase } from "./strings";
 export { ensureString, stringEqualsIgnoreCase } from "./strings";
-
-// ------------------------------------------------------------ //
-// Numbers
-// ------------------------------------------------------------ //
-
-import { ensureNumber, minimumDigits, ordinal } from "./numbers";
 export { ensureNumber, minimumDigits, ordinal } from "./numbers";
-
-// ------------------------------------------------------------ //
-// Arrays
-// ------------------------------------------------------------ //
-
-import { arrayHasElements, prependToArray, appendToArray, removeFromArray, segmentArray, arrayAllMatchFirstElement } from "./arrays";
 export { arrayHasElements, prependToArray, appendToArray, removeFromArray, segmentArray, arrayAllMatchFirstElement } from "./arrays";
-
-import { numberSort, stringSort } from "./sorting";
 export { numberSort, stringSort } from "./sorting";
-
-// ------------------------------------------------------------ //
-// Dates
-// ------------------------------------------------------------ //
-
-import { parseDate } from "./dates";
 export { parseDate } from "./dates";
-
-// ------------------------------------------------------------ //
-// HTML Elements
-// ------------------------------------------------------------ //
-
-import { setTotalWidth, setTotalHeight } from "./html";
 export { setTotalWidth, setTotalHeight } from "./html";
