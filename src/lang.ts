e38c316e74ac64ec56a2ea0dc5d270f123f6e4a2
Alexander <protonull@protonmail.com>
2023-04-24 15:55:16 +0100

    Add inlineThrow and deprecate requireExistence

--- src/lang.ts
+++ src/lang.ts
@@ -8,14 +8,29 @@ export function exists(
 }
 
 /**
+ * Polyfill of throw-expressions.
+ * https://www.proposals.es/proposals/throw%20expressions
+ */
+export function inlineThrow<T>(
+    error: any
+): T {
+    throw error;
+}
+
+/**
  * Requires that a given thing has a value, or throws an error.
+ *
+ * @deprecated Please use inlineThrow instead.
+ * @example
+ * // Don't do this anymore:
+ * const example = requireExistence(someValue, "someValue must exist!");
+ *
+ * // Do this instead:
+ * const example = someValue ?? inlineThrow(new Error("someValue must exist!"));
  */
 export function requireExistence<T>(
     thing: T,
     message: string = "Given thing must exist!"
 ): T {
-    if (exists(thing)) {
-        return thing;
-    }
-    throw new Error(message);
+    return thing ?? inlineThrow(new Error(message));
 }
